7107ee2744c800d41bb28c58055396cbb0f5c69d
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-02-02 09:32:20 -0800

    [Nikita] Refactor.Models.py in Notes

---
+++ Notes/models.py
@@ -0,0 +1,32 @@
+
+from user.models import User
+
+from django.db import models
+
+# Create your models here.
+class Labels(models.Model):
+    name = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return str(self.name)
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    collaborator = models.ManyToManyField(User, related_name='collaborator')
+    label = models.ManyToManyField(Labels)
+    isArchive = models.BooleanField(default=False)
+    isTrash = models.BooleanField(default=False)
+    color = models.CharField(max_length=10, null=True, blank=True)
+    reminder = models.DateTimeField(null=True, blank=True)
+    image = models.ImageField(upload_to='images',null=True,blank=True)
+    def __str__(self):
+        return str(self.title)
+
+
+
