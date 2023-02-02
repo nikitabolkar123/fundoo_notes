0fd081ffb6df0488195942d0aff8cb513a2512b1
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-02-02 09:25:48 -0800

    [Nikita] Refactor.Models in User

--- user/models.py
+++ user/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    phnno = models.IntegerField()
-    city = models.CharField(max_length=100)
+    city = models.CharField(max_length=100,null=True)
+    phnno = models.IntegerField(null=True)
