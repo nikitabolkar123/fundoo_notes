31ecd7b9c7fbea304e021f5eb30b9342d248d357
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-02-04 22:47:48 -0800

    [Nikita] Refactor.views.py in Notes app

--- Notes/views.py
+++ Notes/views.py
@@ -2,17 +2,18 @@ from urllib import request
 from rest_framework import viewsets
 from django.shortcuts import render
 from rest_framework.views import APIView
-from Notes.serializers import NotesSerializer
-from Notes.models import Note
+from Notes.serializers import NotesSerializer, LabelsSerializer
+from Notes.models import Note, Labels
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
-# Create your views here.
 import user.serializers
-from Notes.serializers import NotesSerializer
-#
+
+
+# Create your views here.
 class NotesAPIViews(APIView):
     def post(self,request):
-        serializer =NotesSerializer(data=request.data)
+        print(request.data)
+        serializer =NotesSerializer(data=request.data,context=request.data.get('label'))
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message":"Registered Successfully:", "data":serializer.data,"status":201},status=201)
@@ -49,4 +50,62 @@ class NotesAPIViews(APIView):
             return Response({"message": "deleted successfully", "status": 204})
         except Exception as e:
             return Response({'message':str(e)},status=400)
+
+print('*******************************************************')
+class LabelsAPIViews(APIView):
+    def post(self, request):
+        try:
+            serializer = LabelsSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"message": "Label Added Successfully", "data": serializer.data, "status": 201}, status=201)
+        except Exception as e:
+            return Response({'message': str(e)}, status=400)
+
+    def get(self, request):
+        try:
+            user_id = request.data.get('user')
+            labels = Labels.objects.filter(user=user_id)
+            serializer = LabelsSerializer(labels, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'message': str(e)}, status=400)
+
+    def put(self, request):
+        try:
+            user_id = request.data.get('user')
+            # we have to update the label name so we cannot take it as unique value
+            pk = request.data.get('id')
+            labels = Labels.objects.get(user_id=user_id, id=pk)
+            serializer = LabelsSerializer(labels, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({'message': 'Labels updated successfully!', 'Data': serializer.data})
+
+        except Exception as e:
+            return Response({'message': str(e)}, status=400)
+
+    def delete(self, request):
+        try:
+            user_id = request.data.get('user')
+            pk = request.data.get('id')
+            labels = Labels.objects.get(user_id=user_id, id=pk)
+            labels.delete()
+            return Response({"Message": "Labels Deleted Successfully"}, status=204)
+        except Exception as e:
+            return Response({'message': str(e)}, status=400)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
