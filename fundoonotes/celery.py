c9c3b239829ba44e66ea625ba0d4d7b765ac4ccd
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-02-17 15:15:54 +0530

    [Nikita].added celery.py

--- fundoonotes/celery.py
+++ fundoonotes/celery.py
@@ -4,16 +4,11 @@ from celery import Celery
 from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','fundoonotes.settings')
 app= Celery('fundoonotes')
+app.loader.override_backends['django-db'] = 'django_celery_results.backends.database:DatabaseBackend'
 app.conf.enable_utc=False
 app.conf.update(timezone='Asia/Kolkata')
 app.config_from_object(settings,namespace='CELERY')
-app.conf.beat_schedule={
-    # 'send-mail-every-day-8':{
-    #     'task':'send_mail_app.tasks.send_mail_func',
-    #     'schedule':crontab(hour=17,minute=7),
-    #      # 'args':(2)
-    }
-
+app.conf.beat_schedule={}
 
 app.autodiscover_tasks()
 @app.task(bind=True)
