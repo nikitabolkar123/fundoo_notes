58c36e4c0d990a6067ea7f3f50754aacfbab5566
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-02-17 21:26:34 +0530

    [Nikita] Refactor. FundooNote Project

--- user/views.py
+++ user/views.py
@@ -6,20 +6,37 @@ from rest_framework import permissions
 from rest_framework.views import APIView
 from user.models import User
 from user.serializers import RegistrationSerializer,LoginSerializer
+import logging
+logging.basicConfig(
+    filename='exceptions.log',
+    level=logging.ERROR,
+    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+)
 
 # Create your views here.
 class UserRegistration(APIView):
     serializer_class=RegistrationSerializer
     def post(self,request):
-        serializer=RegistrationSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"message": "User Registration Successfully", "data": serializer.data, "status": 201},status=201)
+        try:
+            serializer=RegistrationSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"message": "User Registration Successfully", "data": serializer.data, "status": 201},status=201)
+        except Exception as e:
+            logging.exception(e)
+            return Response({'message': str(e)}, status=400)
 
-    def get(self, request):
+
+def get(self, request):
+    try:
         user = User.objects.all()
         serializer = RegistrationSerializer(user, many=True)
         return Response(serializer.data)
+    except Exception as e:
+        logging.exception(e)
+        return Response({'message': str(e)}, status=400)
+
+
 class Login(APIView):
     serializer_class=LoginSerializer
 
