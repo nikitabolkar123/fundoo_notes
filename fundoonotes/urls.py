32f854752bc0ed72cfcd8a4277f24c6b0b8cb69d
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-02-19 23:17:20 +0530

    [Nikita] Added urls.py

--- fundoonotes/urls.py
+++ fundoonotes/urls.py
@@ -13,10 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Docs",
+      default_version='v1',
+      description="Fundoo Note Registration And Login",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="nikitabolkar9975@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('notes/', include('notes.urls')),
