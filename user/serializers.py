e261938de496abbc00cb84374a40bb38f03453bf
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-02-19 23:16:38 +0530

    [Nikita] Added Serializer for user model

--- user/serializers.py
+++ user/serializers.py
@@ -4,15 +4,26 @@ from django.forms import forms
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from user.models import User
+import logging
+
+logging.basicConfig(filename='fundoo_note.log', encoding='utf-8', level=logging.DEBUG,
+                    format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logger = logging.getLogger()
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'username', 'phnno', 'password', 'city']
         extra_kwargs = {'password': {'write_only':True}}
-
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        """
+        Create and return a new user instance, given the validated data.
+        """
+        try:
+            return User.objects.create_user(**validated_data)
+        except Exception as ex:
+            logger.exception(ex)
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
