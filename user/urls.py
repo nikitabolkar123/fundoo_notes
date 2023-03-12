b1e4ad45d2b91b539087bc257d8d95f16bd0cc1d
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-03-12 23:37:04 +0530

    [Nikita] Added. jinja templates for fundoo notes user registration and login

--- user/urls.py
+++ user/urls.py
@@ -2,15 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     # path('userregistration/', views.UserRegistration.as_view(), name = 'User_registration'),
-     # path('', views.Login.as_view(), name='login'),
+     path('userregistration/', views.UserRegistration.as_view(), name = 'User_registration'),
+     # path('', views.Login.as_view(), name='authuser_login'),
      # path('logout/', views.Logout.as_view(), name='user_logout'),
-     path('', views.UserLogin.as_view(), name='login'),
+     path('', views.UserLogin.as_view(), name='user_login'),
+     path('login', views.UserLogin.as_view(), name='login'),
      path('register/', views.UserRegistrationTemp.as_view(), name='register'),
      path('profile/', views.User_Profile.as_view(), name='profile'),
      path('logout/', views.UserLogout.as_view(), name='logout')
 ]
-#      path('login_template/', views.LoginTemplate.as_view(), name='loginview'),
-#      path('registration_template/', views.UserRegistrationTemp.as_view(), name='registerview'),
-#      path('userprofile/', views.UserProfile.as_view(), name='User_Profile')
-# ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,17 @@
-from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import View
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from logconfig.logger import get_logger
 from user.models import User
 from user.serializers import RegistrationSerializer, LoginSerializer
-
 logger = get_logger()
 
 
@@ -63,3 +67,57 @@ class Logout(APIView):
             logout(request)
             return Response({"Message": "Logout Successfully"})
         return Response({"Message": "User already logout"})
+
+#
+class UserRegistrationTemp(View):
+    @csrf_exempt
+    def post(self, request):
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            email = request.POST.get('email')
+            phnno = request.POST.get('phnno')
+            city = request.POST.get('city')
+            user = User.objects.create_user(username=username, password=password, first_name=first_name,
+                                            last_name=last_name, email=email, phnno=phnno, city=city)
+            messages.success(request,"Your account has been created successfully")
+            return redirect('login')
+        except Exception as e:
+            logger.exception(e)
+            return render(request, 'user/registration.html')
+
+    def get(self, request):
+        return render(request, 'user/registration.html')
+
+
+class UserLogin(View):
+    name_of_template = 'user/login.html'
+    @method_decorator(sensitive_post_parameters('password'))
+    def post(self, request):
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+            else:
+                return render(request, self.name_of_template)
+        except Exception as e:
+            logger.exception(e)
+            return render(request, self.name_of_template)
+
+    def get(self, request):
+        return render(request, self.name_of_template) #render login.html page
+
+
+class User_Profile(View):
+    def get(self, request):
+        context = {'heading': "Hey.....WELCOME TO FUNDOO NOTE", 'user': request.user}
+        return render(request, 'user/user_profile.html', context)
+class UserLogout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')  # it will redirect to login page
